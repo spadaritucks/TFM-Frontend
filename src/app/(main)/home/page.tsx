c2d9779b6b1f98e0ef945b29ea8b57630cf9606f
spadaritucks <thiago.spadari02@gmail.com>
2025-08-05 13:17:02 -0300

    Adiciona nova página inicial com componente de cabeçalho e estilos correspondentes.

---
+++ src/app/(main)/home/page.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/header/component"
+import "./styles.css"
+
+
+export default function Home() {
+    return (
+
+        <div>
+            <Header />
+        </div>
+    )
+}
---
+++ src/components/header/component.tsx
@@ -0,0 +1,7 @@
+'use client'
+
+export default function Header() {
+    return (
+        <div>Hello World</div>
+    )
+}
