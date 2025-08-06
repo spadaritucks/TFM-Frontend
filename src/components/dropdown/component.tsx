05d42d3c1b3e0c2c0bf3940e0a2a8b8addf73eb9
spadaritucks <thiago.spadari02@gmail.com>
2025-08-06 14:36:55 -0300

    Adiciona a biblioteca lucide-react e atualiza o componente de cabeçalho para incluir ícones de navegação. Melhora o estilo do cabeçalho e do dropdown de ações do usuário.

---
+++ src/components/dropdown/component.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import './styles.css'
+import { ArrowDown } from 'lucide-react';
+import Image from 'next/image';
+
+interface DropdownProps {
+
+    itens: { label: string, onClick: () => void }[]
+    name: string
+    actionsName?: string
+    image?: string
+}
+
+export default function Dropdown({ itens, name, image, actionsName }: DropdownProps) {
+
+    const [open, setOpen] = useState<boolean>(false)
+
+
+    return (
+        <div className='root'>
+
+            <div className='trigger' onClick={() => setOpen(!open)}>
+                {image ? <Image width={50} height={50} src={image} className='avatar' alt='' /> : null}
+                <p>{name}</p>
+                <ArrowDown color="#7C7C8A" width={20} height={20} />
+            </div>
+
+            <div className={`content ${open ? "open" : ""}`}>
+                {actionsName && <div className='dropdown-header'>
+                    <p>{actionsName}</p>
+                </div>}
+                <div className='items'>
+                    {itens.map((item, index) => (<div className='item' key={index} onClick={item.onClick}>{item.label}</div>))}
+                </div>
+            </div>
+
+        </div>
+    );
+}
--- src/components/header/component.tsx
+++ src/components/header/component.tsx
@@ -1,27 +1,41 @@
 'use client'
-import Image from 'next/image'
 import './styles.css'
 import { UserResponseDTO } from '@/@types/DTOs/Users/UserResponseDTO'
 import Link from 'next/link'
-import { usePathname } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
+import { ArrowLeftRight, Goal, Home, LayoutDashboard } from 'lucide-react'
+import Dropdown from '../dropdown/component'
+
 
 interface HeaderProps {
-    user : UserResponseDTO
+    user: UserResponseDTO
 }
 
-export default function Header({user} : HeaderProps) {
+export default function Header({ user }: HeaderProps) {
 
     const pathname = usePathname()
+    const router = useRouter()
+
+    const username = user.name.split(" ")
+    const name = username[0]
 
     return (
         <header>
             <nav>
-                <Link href={'/home'} aria-current={pathname === "/home" ? 'page' : undefined}>Home</Link>
-                <Link href={'/transactions'} aria-current={pathname === "/transactions" ? 'page' : undefined}>Transações</Link>
-                <Link href={'/goals'} aria-current={pathname === "/goals" ? 'page' : undefined}>Metas</Link>
-                <Link href={'/dashboard'} aria-current={pathname === "/dashboard" ? 'page' : undefined}>Dashboard</Link>
+                <Link href={'/home'} aria-current={pathname === "/home" ? 'page' : undefined}><Home /> Home</Link>
+                <Link href={'/transactions'} aria-current={pathname === "/transactions" ? 'page' : undefined}><ArrowLeftRight /> Transações</Link>
+                <Link href={'/goals'} aria-current={pathname === "/goals" ? 'page' : undefined}> <Goal /> Metas</Link>
+                <Link href={'/dashboard'} aria-current={pathname === "/dashboard" ? 'page' : undefined}><LayoutDashboard /> Dashboard</Link>
             </nav>
-            <Image width={60} height={60} src={user.userPhoto} className='avatar'  alt=''/>
+            <Dropdown
+                    name={name}
+                    itens={[
+                        { label: "Editar Usuario", onClick: () => router.push("/edit") },
+                        { label: "Logout", onClick: () => null }
+                    ]}
+                    image={user.userPhoto}
+                    actionsName='Ações do Usuario'
+                />
         </header>
     )
 }
